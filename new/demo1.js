0219c31eb41dd31aade627e76e34d21f9eddedab
David Lippman <drlippman@yahoo.com>
2019-04-25 10:22:51 -0700

    Build new MQ editor functions

---
+++ new/demo1.js
@@ -0,0 +1,295 @@
+/*
+  This object contains the layouts we'll be using. These are exposed by the
+  getLayoutstyle and getLayout functions it returns.
+  We'll pass those functions to the MQeditor in the config.
+ */
+var myMQeditor = (function($) {
+  var mobileLayout = [ //row 0
+    [ //array of columns
+      {s:2},
+      {l:'\\left(\\right)', c:'t', w:'('},
+      {l:'x^{}', c:'t', w:'^'},
+      {s:.25},
+      {b:'7'},
+      {b:'8'},
+      {b:'9'},
+      {l:'\\frac{}{}', c:'t', w:'/'},
+      {s:.75},
+      {b:'&uarr;', c:'k', w:'Up'},
+      {s:.5}
+    ],
+    [ //row 1
+      {p:'Funcs', s:2, 'panel': [
+        [
+          {l:'\\log', c:'f'},
+          {l:'\\ln', c:'f'},
+          {l:'\\log_{}', c:'f'},
+          {l:'e^{}', c:'t', w:'e^'},
+          {s:1},
+          {s:1}
+        ],
+        [
+          {l:'\\sin', c:'f'},
+          {l:'\\cos', c:'f'},
+          {l:'\\tan', c:'f'},
+          {l:'\\sec', c:'f'},
+          {l:'\\csc', c:'f'},
+          {l:'\\cot', c:'f'}
+        ],
+        [
+          {l:'\\sin^{-1}', c:'f'},
+          {l:'\\cos^{-1}', c:'f'},
+          {l:'\\tan^{-1}', c:'f'},
+          {l:'\\sinh', c:'f'},
+          {l:'\\cosh', c:'f'},
+          {l:'\\tanh', c:'f'}
+        ]
+      ]},
+      {l:'\\pi'},
+      {l:'\\sqrt{}', c:'c', w:'sqrt'},
+      {s:.25},
+      {b:'4'},
+      {b:'5'},
+      {b:'6'},
+      {b:'*'},
+      {s:.25},
+      {b:'&larr;', c:'k', w:'Left'},
+      {b:'&rarr;', c:'k', w:'Right'}
+    ],
+    [ //row 2
+      {s:2},
+      {l:'\\infty'},
+      {l:'\\sqrt[n]{}', c:'c', w:'nthroot'},
+      {s:.25},
+      {b:'1'},
+      {b:'2'},
+      {b:'3'},
+      {b:'-'},
+      {s:.75},
+      {b:'&darr;', c:'k', w:'Down'},
+      {s:.5}
+    ],
+    [ //row 3
+      {p:'ABC', s:2, 'left': 0, 'panel': [
+        [
+          {p:'q'},{p:'w'},{p:'e'},{p:'r'},{p:'t'},
+          {p:'y'},{p:'u'},{p:'i'},{p:'o'},{p:'p'}
+        ],
+        [
+          {s:.5},{p:'a'},{p:'s'},{p:'d'},{p:'f'},{p:'g'},
+          {p:'h'},{p:'j'},{p:'k'},{p:'o'},{s:.5}
+        ],
+        [
+          {b:'&#8679;', c:'shift', s:1.5},
+          {p:'z'},{p:'x'},{p:'c'},{p:'v'},{p:'b'},
+          {p:'n'},{p:'m'},
+          {b:'&#x232B;', c:'k', w:'Backspace', s:1.5}
+        ],
+        [
+          {s:2},
+          {l:'\\left[\\right]', c:'i', w:'[]'},
+          {l:'\\langle{}', c:'c', w:'\\langle'},
+          {p:'%'},
+          {p:','},
+          {b:'&larr;', c:'k', w:'Left'},
+          {b:'&rarr;', c:'k', w:'Right'}
+        ]
+      ]},
+      {p:'DNE', 'sm':1},
+      {l:'\\left|\\right|', c:'t', w:'|'},
+      {s:.25},
+      {b:'0'},
+      {b:'.'},
+      {b:'='},
+      {b:'+'},
+      {s:.75},
+      {b:'&#x232B;', c:'k', w:'Backspace'},
+      {s:.5}
+    ]
+  ];
+  var ineqPanel = {b:'&le;', s:2, 'panel': [
+    [
+      {l:'\\lt'},
+      {l:'\\gt'},
+      {l:'\\le'},
+      {l:'\\ge'}
+    ],
+    [
+      {p:'or'},
+      {p:'DNE', 'sm':1},
+      {p:'all reals', c:'w', w:'\\text{all reals}', s:2}
+    ]
+  ]};
+  var intervalPanel = {l:'\\left({},{}\\right]', s:2, 'panel': [
+    [
+      {l:'\\left(\\right)', c:'i', w:'()'},
+      {l:'\\left[\\right]', c:'i', w:'[]'},
+      {l:'\\left(\\right]', c:'i', w:'(]'},
+      {l:'\\left[\\right)', c:'i', w:'[)'}
+    ],
+    [
+      {l:'\\infty'},
+      {l:'-\\infty', c:'w'},
+      {l:'\\cup'},
+      {s:1}
+    ]
+  ]};
+  var underLayout = [
+    [ //row 0
+      {p:'Funcs', s:2, 'panel': [
+        [
+          {l:'\\log', c:'f'},
+          {l:'\\ln', c:'f'},
+          {l:'\\log_{}', c:'f'},
+          {l:'e^{}', c:'t', w:'e^'},
+          {s:1},
+          {s:1}
+        ],
+        [
+          {l:'\\sin', c:'f', sm:1},
+          {l:'\\cos', c:'f', sm:1},
+          {l:'\\tan', c:'f', sm:1},
+          {l:'\\sec', c:'f', sm:1},
+          {l:'\\csc', c:'f', sm:1},
+          {l:'\\cot', c:'f', sm:1}
+        ],
+        [
+          {l:'\\sin^{-1}', c:'f', sm:1},
+          {l:'\\cos^{-1}', c:'f', sm:1},
+          {l:'\\tan^{-1}', c:'f', sm:1},
+          {l:'\\sinh', c:'f', sm:1},
+          {l:'\\cosh', c:'f', sm:1},
+          {l:'\\tanh', c:'f', sm:1}
+        ]
+      ]},
+      {l:'\\frac{}{}', c:'t', w:'/'},
+      {l:'x^{}', c:'t', w:'^'},
+      {l:'x_{}', c:'t', w:'_'},
+      {l:'\\sqrt{}', c:'c', w:'sqrt'},
+      {l:'\\sqrt[n]{}', c:'c', w:'nthroot'},
+      {s:.25},
+      {b:'&uarr;', c:'k', w:'Up'},
+      {b:'&darr;', c:'k', w:'Down'},
+    ],
+    [ //row 1
+      {s:2},
+      {l:'\\left|\\right|', c:'t', w:'|'},
+      {l:'\\left(\\right)', c:'t', w:'('},
+      {l:'\\pi'},
+      {l:'\\infty'},
+      {p:'DNE', 'sm':2},
+      {s:.25},
+      {b:'&larr;', c:'k', w:'Left'},
+      {b:'&rarr;', c:'k', w:'Right'}
+    ],
+    [ //row 2
+      {s:7.25},
+      {b:'&#x232B;', s:2, c:'k', w:'Backspace'}
+    ]
+  ];
+
+  function getLayout(el) {
+    var layoutstyle = MQeditor.getLayoutstyle();
+    var baseid = el.id.substring(8);
+    var textel = $('#'+baseid);
+    //TODO: fix this - need to get from params
+    var vars = ['x','y'];
+    var qtype = 'calcinterval';
+    var calcformat = 'inequality';
+    var baselayout;
+    if (layoutstyle === 'OSK') {
+      baselayout = mobileLayout.slice();
+      if (vars.length > 0) {
+        // replace spacer with vars buttons or panel control
+        baselayout[0] = getVarsButtons(vars).concat(baselayout[0].slice(1));
+      }
+      if (qtype=='calcinterval') {
+        if (calcformat.match(/inequality/)) {
+          baselayout[2][0] = ineqPanel;
+        } else {
+          baselayout[2][0] = intervalPanel;
+        }
+        baselayout[2][0].left = 100*2/10.5;
+      }
+    } else { // under layout
+      baselayout = underLayout.slice();
+      if (qtype=='calcinterval') {
+        if (calcformat.match(/inequality/)) {
+          baselayout[1][0] = ineqPanel;
+        } else {
+          baselayout[1][0] = intervalPanel;
+        }
+        baselayout[1][0].left = 100*2/9.25;
+      }
+    }
+    return baselayout;
+  }
+
+  function getVarsButtons(vars) {
+    if (vars.length<3 &&
+      vars[0].length<3 &&
+      vars[1].length<3
+    ) {
+      //put them as regular buttons.
+      if (vars.length==1) {
+        return [{'b':vars[0]},{'s':1}];
+      } else {
+        return [{'b':vars[0]},{'b':vars[1]}];
+      }
+    } else {
+      var perrow = Math.min(row.length-2,Math.max(4, Math.ceil(btn.vars.length/4)));
+      var subarr = [];
+      var cnt=0;
+      for (nr=0;nr<Math.ceil(btn.vars.length/perrow);nr++) {
+        subarr[nr] = [];
+        for (nc=0;nc<perrow;nc++) {
+          if (cnt<btn.vars.length) {
+            subarr[nr][nc] = {'b':btn.vars[cnt]};
+          } else {
+            subarr[nr][nc] = {'s':1};
+          }
+          cnt++;
+        }
+      }
+      return [{'p':'Vars', 's':2, 'panel':subarr.slice()}];
+    }
+  }
+
+  return {
+    getLayout: getLayout
+  }
+})(jQuery);
+
+
+/*
+  This code initializes everything
+ */
+// Tell the editor to use our functions above to generate layout
+MQeditor.setConfig({
+  getLayout: myMQeditor.getLayout
+});
+
+// set the default MathQuill config.
+var MQ = MathQuill.getInterface(MathQuill.getInterface.MAX);
+MQ.config({
+  leftRightIntoCmdGoes: 'up',
+  supSubsRequireOperand: true,
+  autoSubscriptNumerals: true,
+  autoCommands: 'pi theta sqrt oo',
+  autoParenOperators: true,
+  addCommands: {'oo': ['VanillaSymbol', '\\infty ', '&infin;']},
+});
+
+// add button to toggle MQ on inputs
+$(function() {
+  $("input[type=text]").each(function(i,el) {
+    var btn = $("<button/>", {
+      type:"button",
+      text:"MQ",
+      click: function() {
+        MQeditor.toggleMQ(el);
+      }
+    });
+    $(this).after(btn);
+  });
+});
---
+++ new/mqeditor.js
@@ -0,0 +1,449 @@
+/*
+  Equation Editor palette tool for MathQuill
+  (c) David Lippman 2019
+  Mozilla Public License, v. 2.0: http://mozilla.org/MPL/2.0/
+
+  You can toggle a plain input field into a MathQuill field with attached
+  editor using toggleMQ.  Alternatively if you already have an MathQuill
+  mathfield, you can attach the editor using attachEditor.  If pre-creating
+  mathfields, make sure you use substituteTextarea if your layoutstyle is 'OSK'
+
+  To get anything to display, you'll need to first call setConfig with a config
+  object defining either layout (an array describing the layout) or
+  getLayout, a function that returns a layout array.
+
+ */
+
+var MQeditor = (function($) {
+  var config = {
+    layoutstyle: 'auto',
+    layout: []
+  };
+  var MQconfig = {};
+  var curMQfield = null;
+  var blurTimer = null;
+  var MQ = MathQuill.getInterface(MathQuill.getInterface.MAX);
+
+  /*
+    Config object for MQeditor
+    layoutstyle:
+      "under" for on-focus floating under entry box,
+      "OSK" for on-focus fixed-position on-screen-keyboard
+    layout: array that defines the palette buttons
+    getLayout: a function that returns a layout. Gets passed the MQ element.
+    getLayoutstyle: a function that returns a value for layoutstyle.
+    toMQ:  function to convert text to MQ latex
+    fromMQ: function to convert MQ latex to input
+   */
+  function setConfig(newconfig) {
+    for (var i in newconfig) {
+      config[i] = newconfig[i];
+    }
+  }
+
+  /*
+    MQconfig holds the config used when creating MQ editable fields
+   */
+  function setMQconfig(newconfig) {
+    MQconfig = newconfig;
+  }
+
+  /*
+    Toggles the given input field to/from a MathQuill field
+    and attaches the editor to the field
+    el: the input field node or id of input. Must have a unique id
+    state: (optional) true to toggle to MQ; false to regular input
+   */
+  function toggleMQ(el, state, nofocus) {
+    if (typeof el == 'string') {
+      el = document.getElementById(el);
+    }
+    var newstate = state || (el.type != 'hidden');
+    var textId = el.id;
+    if (newstate === true) { // enable MQ
+      var initval = $(el).attr("type","hidden").val();
+      if (config.hasOwnProperty('toMQ')) { // convert format if needed
+        initval = config.toMQ(initval);
+      }
+      var mqfield = $("#mqinput-"+textId);
+      if (mqfield.length == 0) {  // no existing MQ input
+        var span = $("<span/>", {
+          id: "mqinput-"+textId,
+          class: "mathquill-math-field",
+          text: initval
+        });
+  			span.insertAfter(el);
+        var thisMQconfig = {
+          handlers: {
+            edit: onMQedit
+          }
+        };
+        if (config.hasOwnProperty("getLayoutstyle")) {
+          config.curlayoutstyle = config.getLayoutstyle();
+        } else if (config.layoutstyle == 'auto') {
+          config.curlayoutstyle = getLayoutstyle();
+        } else {
+          config.curlayoutstyle = config.layoutstyle;
+        }
+        if (config.curlayoutstyle == 'OSK') {
+          thisMQconfig.substituteTextarea = function () {
+            var s = document.createElement('span');
+            s.setAttribute('tabindex', 0);
+            return s;
+          };
+          thisMQconfig.keyboardPassthrough = true;
+        }
+        mqfield = MQ.MathField(span[0], MQconfig).config(thisMQconfig);
+        attachEditor(span);
+
+      } else { // has existing MQ input
+        mqfield.show();
+  			mqfield = MQ(mqfield[0]).latex(initval);
+      }
+      if (nofocus !== true) {
+        mqfield.focus();
+      }
+    } else { // disable MQ
+      $(el).attr("type","text").focus();
+      $("#mqinput-"+textId).hide();
+    }
+
+  }
+
+  /*
+    Toggles all inputs matching the selector to/from a MathQuill field
+    state: (optional) true to toggle to MQ; false to regular input
+   */
+  function toggleMQAll(selector, state) {
+    var newstate = state || (el.type != 'hidden');
+    $(selector).each(function(i,el) {
+      toggleMQ(el, newstate, true);
+    });
+  }
+
+  /*
+    Sets onfocus/onblur listeners to show/hide the editor on focus/blur
+    call with the MQ node
+   */
+  function attachEditor(mqel) {
+    // set up editor to display on focus
+    $(mqel).find(".mq-textarea > *")
+      .on('focus.mqeditor', showEditor)
+      .on('blur.mqeditor', function() {
+        blurTimer = setTimeout(hideEditor, 100);
+      });
+  }
+
+  /*
+    Display the editor, rebuilding the panel if needed
+   */
+  function showEditor(event) {
+    clearTimeout(blurTimer);
+    var mqel = $(event.target).closest(".mathquill-math-field");
+    if (curMQfield === null) {
+      // first time through: inject the mqeditor div
+      $("body").append($("<div/>", {id:"mqeditor", class:"mqeditor"}));
+      // prevent clicks in editor from triggering blur in MQ field
+      $("#mqeditor").on("mousedown touchstart", function(evt) {evt.preventDefault();});
+    }
+    // update layoutStyle if needed
+    if (config.hasOwnProperty("getLayoutstyle")) {
+      config.curlayoutstyle = config.getLayoutstyle();
+    } else if (config.layoutstyle == 'auto') {
+      config.curlayoutstyle = getLayoutstyle();
+    } else {
+      config.curlayoutstyle = config.layoutstyle;
+    }
+    if (config.curlayoutstyle === 'OSK') {
+      $("#mqeditor").addClass("fixedbottom");
+    } else {
+      $("#mqeditor").removeClass("fixedbottom");
+    }
+    // see if the field has changed
+    if (curMQfield === null || mqel[0] != curMQfield.el()) {
+      // new field; need to build the panel
+      // update the layout based on the element
+      if (config.hasOwnProperty("getLayout")) {
+        config.layout = config.getLayout(mqel[0]);
+      }
+      // empty existing
+      $("#mqeditor").empty().show();
+      // build new panel
+      buildPanel(document.getElementById("mqeditor"), config.layout);
+      // render buttons then hide subpanels
+      $("#mqeditor .mqed-btn.rend").each(function() {MQ.StaticMath(this, {mouseEvents: false}); });
+      $("#mqeditor .mqed-subpanel").hide();
+    }
+    // now show and position the editor
+    $("#mqeditor").show();
+    positionEditor(mqel);
+    curMQfield = MQ.MathField(mqel[0]);
+  }
+
+  /*
+    Hide the editor
+   */
+  function hideEditor(event) {
+    $("#mqeditor").hide();
+  }
+
+  /*
+    Positions the editor below the MQ field, if layoutstyle dictates
+   */
+  function positionEditor(ref) {
+    if (config.curlayoutstyle == 'under') {
+    	var mqfield = $(ref).closest(".mathquill-math-field");
+    	var offset = mqfield.offset();
+    	var height = mqfield.height();
+      var editorWidth = document.getElementById("mqeditor").offsetWidth;
+      var editorLeft = offset.left;
+      if (editorLeft + editorWidth > document.documentElement.clientWidth) {
+        editorLeft = document.documentElement.clientWidth - editorWidth-5;
+      }
+      // TODO: adjust left if
+    	$("#mqeditor").css("top", offset.top + height + 3).css("left", editorLeft);
+    }
+  }
+  /*
+    Update the editor position, and update the regular input field
+   */
+  function onMQedit(mf) {
+  	var el = mf.el();
+  	positionEditor(el);
+  	if (el.id.match(/mqinput/)) {
+      var latex = mf.latex();
+      if (config.hasOwnProperty('fromMQ')) {
+        //convert to input format
+        latex = config.fromMQ(latex);
+      }
+  		document.getElementById(el.id.substring(8)).value = latex;
+  	}
+  }
+
+  /*
+    The default layoutstyle calculator
+   */
+  function getLayoutstyle() {
+    var width = document.documentElement.clientWidth;
+    if (navigator.userAgent.match(/Android/) ||
+      navigator.userAgent.match(/iPhone/) ||
+      navigator.userAgent.match(/iPad/) ||
+      width < 500
+    ) {
+      return 'OSK';
+    } else {
+      return 'under';
+    }
+  }
+
+  /*
+    Build the editor panel
+  */
+  function buildPanel(baseel, layoutarr) {
+    var row,btn,rowel,btnel,btncont,r,c,cmdtype,cmdval,subpanel,nr,nc,cnt;
+    for (r=0;r<layoutarr.length;r++) {
+      rowel = document.createElement("div");
+      rowel.className = 'mqed-row';
+      row = layoutarr[r];
+      for (c=0;c<row.length;c++) {
+        btn = row[c];
+        btncont = document.createElement("div");
+        btncont.className = "mqed-btn-cont";
+        // set size
+        if (btn.hasOwnProperty('s')) {
+          btncont.style.flexGrow = btn.s;
+        }
+        rowel.appendChild(btncont);
+        if (!btn.hasOwnProperty('l') && !btn.hasOwnProperty('b') && !btn.hasOwnProperty('p')) {
+          //spacer
+          continue;
+        }
+
+        btnel = document.createElement("span");
+        btnel.tabIndex = 0;
+        if (btn.hasOwnProperty('l')) { // latex button
+          btnel.className = "mqed-btn rend";
+          btnel.innerText = btn.l;
+          cmdtype = 'c';
+          cmdval = btn.l.substring(1);
+        } else if (btn.hasOwnProperty('b')) { // rendered text button
+          btnel.className = "mqed-btn rend";
+          btnel.innerHTML = btn.b;
+          cmdtype = 't';
+          cmdval = btn.b;
+        } else { // plain text button
+          btnel.className = "mqed-btn";
+          btnel.innerHTML = btn.p;
+          cmdtype = 't';
+          cmdval = btn.p;
+        }
+        // override default command type
+        if (btn.hasOwnProperty('c')) {
+          cmdtype = btn.c;
+          if (cmdtype == 'shift') {
+            $(btnel).addClass("mqed-shift");
+          }
+        }
+        // make it small; 1 for 90%, 2 for 80%, etc.
+        if (btn.hasOwnProperty('sm')) {
+          btnel.style.fontSize = (100-10*btn.sm) + '%';
+        }
+        // override what to write
+        if (btn.hasOwnProperty('w')) {
+          cmdval = btn.w;
+        }
+        // button controls a subpanel
+        if (btn.hasOwnProperty('panel')) {
+          subpanel = document.createElement("div");
+          subpanel.id = baseel.id + '-sub-'+r+'-'+c;
+          // set left or right position (as percent)
+          if (btn.hasOwnProperty('left')) {
+            subpanel.style.left = btn.left + '%';
+            // ensure control button is still visible over panel
+            btnel.style.zIndex = 200;
+          } else {
+            subpanel.style.left = 'auto';
+          }
+          if (btn.hasOwnProperty('right')) {
+            subpanel.style.right = btn.right + '%';
+          }
+          subpanel.className = "mqed-subpanel";
+          // build subpanel
+          buildPanel(subpanel, btn.panel);
+          baseel.appendChild(subpanel);
+          cmdtype = 'showpanel';
+          cmdval = subpanel.id;
+        }
+        $(btnel).data("cmdtype", cmdtype).data("cmdval", cmdval);
+        $(btnel).on("click touchstart keydown", makeBtnListener(cmdtype, cmdval));
+        btncont.appendChild(btnel);
+      }
+      baseel.appendChild(rowel);
+    }
+  }
+
+  /*
+    Returns a listener function. Used in buildPanel
+  */
+  function makeBtnListener(cmdtype, cmdval) {
+    return function (event) {
+      handleMQbtn(event,cmdtype,cmdval);
+    }
+  }
+
+  /*
+    Handle an editor button click
+  */
+  function handleMQbtn(event, cmdtype, cmdval) {
+    if (event.type=='keydown' && event.key !== 'Enter') {
+      return;
+    }
+    if (event.type=='touchstart') {
+      event.preventDefault();
+    }
+    // return focus to editor
+    //clearTimeout(blurTimer);
+    //curMQfield.focus();
+
+    if (cmdtype == 't') {
+      // do typedText
+      if (cmdval.match(/^[a-zA-Z]$/)) {
+        // check for shift key being active
+        if ($(event.target).closest('.mqed-subpanel').find(".mqed-shift").hasClass("active")) {
+          cmdval = cmdval.toUpperCase();
+        }
+      }
+      curMQfield.typedText(cmdval);
+    } else if (cmdtype == 'c') {
+      // do MQ cmd
+      curMQfield.cmd(cmdval);
+    } else if (cmdtype=='w') {
+      // do MQ write
+      curMQfield.write(cmdval);
+    } else if (cmdtype=='k') {
+      // do MQ keystroke
+      curMQfield.keystroke(cmdval);
+    } else if (cmdtype=='f') {
+      // function; if there's a selection, wrap it in parens and apply function
+  		var sel = curMQfield.getSelection();
+  		if (sel) {
+  			curMQfield.write(cmdval+'\\left('+sel+'\\right)');
+        curMQfield.keystroke('Left');
+  		} else if (cmdval.match(/{}$/)) {
+        curMQfield.typedText(cmdval.replace(/{}$/,''));
+      } else {
+  		  curMQfield.write(cmdval);
+  		}
+    } else if (cmdtype=='sf') {
+      // simple function, that doesn't need parens.
+  		var sel = curMQfield.getSelection();
+  		if (sel) {
+  			curMQfield.write(cmdval+'{'+sel+'}');
+  		} else {
+  		  curMQfield.cmd(cmdval);
+  		}
+  		curMQfield.keystroke('Left');
+    } else if (cmdtype=='i') {
+      // interval: if there's a selection, wrap it
+  		var sel = curMQfield.getSelection();
+      if (!sel) {
+        sel = '';
+      }
+      var leftsym = cmdval.charAt(0);
+      var rightsym = cmdval.charAt(1);
+  		curMQfield.write('\\left'+leftsym+sel+'\\right'+rightsym);
+      if (sel=='') {
+        curMQfield.keystroke('Left');
+      }
+    } else if (cmdtype=='showpanel') {
+      // show a panel
+      var subpanel = document.getElementById(cmdval);
+      var target = $(event.target).closest('.mqed-btn');
+      if (subpanel.style.left === 'auto') {
+        // set left now
+        var targRight = target.parent().position().left + target.parent().width() + 2;
+        subpanel.style.left = (100*targRight/$("#mqeditor").width()) + '%';
+      }
+      if (subpanel.style.display == 'none') { // show it
+        // first hide any already-showing subpanels
+        target.closest(".mqeditor").find(".mqed-subpanel").hide();
+        target.closest(".mqeditor").find(".mqed-btn.active").removeClass("active");
+        // make panel button active
+        target.addClass("active");
+        $(subpanel).show();
+      } else { //hide it
+        target.removeClass("active");
+        $(subpanel).hide();
+      }
+    } else if (cmdtype=='shift') {
+      // enable Shift in alphabet panel
+      var target = $(event.target).closest('.mqed-btn');
+      var subpanel = $(event.target).closest('.mqed-subpanel');
+      var goingActive = !target.hasClass("active");
+      if (goingActive) {
+        target.addClass("active");
+      } else {
+        target.removeClass("active");
+      }
+      // change case of each button label
+      subpanel.find(".mqed-btn").each(function(i,el) {
+        var txt = el.textContent;
+        if (txt.match(/^[a-zA-Z]$/)) {
+          if (goingActive) {
+            el.textContent = el.textContent.toUpperCase();
+          } else {
+            el.textContent = el.textContent.toLowerCase();
+          }
+        }
+      });
+    }
+  }
+  return {
+    setConfig: setConfig,
+    setMQconfig: setMQconfig,
+    toggleMQ: toggleMQ,
+    toggleMQAll: toggleMQAll,
+    attachEditor: attachEditor,
+    getLayoutstyle: getLayoutstyle
+  }
+})(jQuery);
